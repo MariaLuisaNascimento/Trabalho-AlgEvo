dc774cbd211fd5b0df64f4f04fcfafdbd5c4f598
MarileneGarcia <marilenegarcia1998@gmail.com>
2018-10-18 14:34:35 -0300

    Atualizacoes sobre candidatos

--- Classes.cpp
+++ Classes.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
+#include <stdio.h>
+#include <curses.h>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
 
 class Candidato{ 
-    public caracteristicas = new Caracteristicas();
-    public propostas = new Propostas();
-};
+    //Parametros
+    public:
+    double educacao;
+    double educacaoSexual;
+    double saudePublica;
+    double segurancaPublica;
+    double porteArma;
+    double estadoLaico;
+    double estatutoFamilia;
+    double legalizarAborto;
+    double reformaTrabalhista;
+    double reformaAgraria;
+    double legalizarMaconha;
+    double intervencaoMilitar;
+    double escolaSemPartido;
 
-class Caracteristicas{
-    etnia;
-    origemPolitica;
-    origemSocioeconomica;
-    nivelEscolaridade;    
-};
+    //Caracteristicas
+        //Carater politico e economico
+    double autoritario;
+    double libertario;
 
-class Propostas{
-    mudança;
+    double direita;
+    double centro;
+    double esquerda;
+
+        //Carater social
+    double direitaSocial;
+    double esquerdaSocial;
 };
 
 class EstadosEleitores{ 
@@ -36,7 +56,63 @@ class EstadosEleitores{
     int setorMaisImport;
     double criminalidade;
     double taxaHomi;
-    double taxaFEtária;
+    double taxaFEtaria;
 
 
 };
+
+int main(){
+    Candidato* candidatoTeste = new Candidato;
+    srand( (unsigned)time(NULL) );
+    
+    //Inicializando os parametros
+    candidatoTeste -> educacao = (((double)((rand()%101))/100));
+    candidatoTeste -> educacaoSexual = (((double)((rand()%101))/100));
+    candidatoTeste -> saudePublica = (((double)((rand()%101))/100));
+    candidatoTeste -> segurancaPublica = (((double)((rand()%101))/100));
+    candidatoTeste -> porteArma = (((double)((rand()%101))/100));
+    candidatoTeste -> estadoLaico = (((double)((rand()%101))/100));
+    candidatoTeste -> estatutoFamilia = (((double)((rand()%101))/100));
+    candidatoTeste -> legalizarAborto = (((double)((rand()%101))/100));
+    candidatoTeste -> reformaTrabalhista = (((double)((rand()%101))/100));
+    candidatoTeste -> reformaAgraria = (((double)((rand()%101))/100));
+    candidatoTeste -> legalizarMaconha = (((double)((rand()%101))/100));
+    candidatoTeste -> intervencaoMilitar = (((double)((rand()%101))/100));
+    candidatoTeste -> escolaSemPartido = (((double)((rand()%101))/100));
+
+    //Carater politico e economico
+        
+        //Decobrir se um candidato é autoritario ou liberal
+    double criteriosAutoritario = (candidatoTeste -> saudePublica + candidatoTeste -> segurancaPublica + candidatoTeste -> estatutoFamilia +
+                                    + candidatoTeste -> reformaTrabalhista + candidatoTeste -> reformaAgraria + candidatoTeste -> intervencaoMilitar +
+                                    + candidatoTeste -> escolaSemPartido);
+    candidatoTeste -> autoritario = (criteriosAutoritario/7); // 7 é o numero de caracteristicas que o definem, normalizar entre 0 e 1
+    printf("%lf \n", candidatoTeste -> autoritario);
+
+    double criteriosLibertario = (candidatoTeste -> porteArma + candidatoTeste -> estadoLaico + candidatoTeste -> legalizarAborto + candidatoTeste -> legalizarMaconha);
+    candidatoTeste -> libertario = (criteriosLibertario/4);
+    printf("%lf \n", candidatoTeste -> libertario); // 4 é o numero de caracteristicas que definem, normalizar entre 0 e 1
+
+    if(candidatoTeste -> autoritario > candidatoTeste -> libertario)
+        printf("O candidato é autoritario\n");
+    else 
+        printf("O candidato é libertario\n");
+
+        //Descobrir se um candidato é de direita, centro ou esquerda
+    double criteriosDireita = (candidatoTeste -> estatutoFamilia + candidatoTeste -> reformaTrabalhista + candidatoTeste -> intervencaoMilitar + 
+                                + candidatoTeste -> escolaSemPartido); 
+    candidatoTeste -> direita = (criteriosDireita/4); // 4 é o numero de caracteristicas que o definem, normalizar entre 0 e 1
+    printf("%lf \n", candidatoTeste -> direita);
+
+    double criteriosCentro = (0.0); //PROBLEMINHA: NAO HA CRITERIOS QUE DEFINEM O CANDIDATO DE CENTRO
+
+    double criteriosEsquerda = (candidatoTeste -> educacaoSexual + candidatoTeste -> saudePublica + candidatoTeste -> segurancaPublica + 
+                                + candidatoTeste -> estadoLaico + candidatoTeste -> legalizarAborto + candidatoTeste -> reformaAgraria);
+    candidatoTeste -> esquerda = (criteriosEsquerda /6);
+    printf("%lf \n", candidatoTeste -> esquerda); // 6 é o numero de caracteristicas que o definem, normalizar entre 0 e 1
+
+    
+
+
+
+}
