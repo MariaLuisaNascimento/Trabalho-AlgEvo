b586020a4f534bff692c5d437564039f8cd2dcb5
MarileneGarcia <marilenegarcia1998@gmail.com>
2018-10-22 14:23:53 -0300

    Finalizando os candidatos

--- Classes.cpp
+++ Classes.cpp
@@ -65,7 +65,7 @@ int main(){
     Candidato* candidatoTeste = new Candidato;
     srand( (unsigned)time(NULL) );
     
-    //Inicializando os parametros
+    //---- Inicializando os parametros do candidato ----- (acho que a evolução sera neles)
     candidatoTeste -> educacao = (((double)((rand()%101))/100));
     candidatoTeste -> educacaoSexual = (((double)((rand()%101))/100));
     candidatoTeste -> saudePublica = (((double)((rand()%101))/100));
@@ -80,9 +80,9 @@ int main(){
     candidatoTeste -> intervencaoMilitar = (((double)((rand()%101))/100));
     candidatoTeste -> escolaSemPartido = (((double)((rand()%101))/100));
 
-    //Carater politico e economico
+    //---- Carater politico e economico ----
         
-        //Decobrir se um candidato é autoritario ou liberal
+        //---- Decobrir se um candidato é autoritario ou liberal ----
     double criteriosAutoritario = (candidatoTeste -> saudePublica + candidatoTeste -> segurancaPublica + candidatoTeste -> estatutoFamilia +
                                     + candidatoTeste -> reformaTrabalhista + candidatoTeste -> reformaAgraria + candidatoTeste -> intervencaoMilitar +
                                     + candidatoTeste -> escolaSemPartido);
@@ -94,25 +94,55 @@ int main(){
     printf("%lf \n", candidatoTeste -> libertario); // 4 é o numero de caracteristicas que definem, normalizar entre 0 e 1
 
     if(candidatoTeste -> autoritario > candidatoTeste -> libertario)
-        printf("O candidato é autoritario\n");
+        printf("O candidato tem um viés político e economico autoritario\n");
     else 
-        printf("O candidato é libertario\n");
+        printf("O candidato tem um viés político e economico libertario\n");
 
-        //Descobrir se um candidato é de direita, centro ou esquerda
+        //---- Descobrir se um candidato é de direita, centro ou esquerda -----
     double criteriosDireita = (candidatoTeste -> estatutoFamilia + candidatoTeste -> reformaTrabalhista + candidatoTeste -> intervencaoMilitar + 
                                 + candidatoTeste -> escolaSemPartido); 
     candidatoTeste -> direita = (criteriosDireita/4); // 4 é o numero de caracteristicas que o definem, normalizar entre 0 e 1
-    printf("%lf \n", candidatoTeste -> direita);
-
-    double criteriosCentro = (0.0); //PROBLEMINHA: NAO HA CRITERIOS QUE DEFINEM O CANDIDATO DE CENTRO
-
+    printf("%lf \n", candidatoTeste -> direita); 
+    
     double criteriosEsquerda = (candidatoTeste -> educacaoSexual + candidatoTeste -> saudePublica + candidatoTeste -> segurancaPublica + 
                                 + candidatoTeste -> estadoLaico + candidatoTeste -> legalizarAborto + candidatoTeste -> reformaAgraria);
     candidatoTeste -> esquerda = (criteriosEsquerda /6);
     printf("%lf \n", candidatoTeste -> esquerda); // 6 é o numero de caracteristicas que o definem, normalizar entre 0 e 1
 
+    if(candidatoTeste -> direita > 0.5 && candidatoTeste -> esquerda > 0.5){
+        if(candidatoTeste -> direita > candidatoTeste -> esquerda)
+            printf("O candidato tem um viés político e economico de direita\n");
+        else
+            printf("O candidato tem um viés político e economico de esquerda\n");
+    }
+
+    else if(candidatoTeste -> direita > 0.5 || candidatoTeste -> esquerda > 0.5){
+        if(candidatoTeste -> direita > 0.5)
+            printf("O candidato tem um viés político e economico de direita\n");
+        else
+            printf("O candidato tem um viés político e economico de esquerda\n");
+    }
+    else
+        printf("O candidato tem um viés político e economico de centro\n");
+
+
+    //---- Carater Social ----
+
+    double criteriosDireitaSocial = (candidatoTeste -> segurancaPublica + candidatoTeste -> porteArma + candidatoTeste -> estatutoFamilia +
+                                    + candidatoTeste -> reformaTrabalhista + candidatoTeste -> intervencaoMilitar + candidatoTeste -> escolaSemPartido);
+    candidatoTeste -> direitaSocial = (criteriosDireitaSocial/6); // 6 é o numero de caracteristicas que o definem, normalizar entre 0 e 1
+    printf("%lf\n",candidatoTeste -> direitaSocial);
+
+    double criteriosEsquerdaSocial = (candidatoTeste -> educacao + candidatoTeste -> educacaoSexual + candidatoTeste -> saudePublica + candidatoTeste -> estadoLaico +
+                                    + candidatoTeste -> legalizarAborto + candidatoTeste -> legalizarMaconha);
+    candidatoTeste -> esquerdaSocial = (criteriosEsquerdaSocial/6); // 6 é o numero de caracteristicas que o definem, normalizar entre 0 e 1
+    printf("%lf\n",candidatoTeste -> esquerdaSocial);
+
+    if(candidatoTeste -> direitaSocial > candidatoTeste -> esquerdaSocial)
+        printf("O candidato tem um viés social de direita\n");
+    else 
+        printf("O candidato tem um viés social de esquerda\n");
+
+    //VOU COMEÇAR A SETAR OS ESTADOS, SE VC FOR MEXER, ME MANDA MENSAGEM PLEASE
     
-
-
-
 }
